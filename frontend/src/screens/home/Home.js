249059c570a7e6e3c82be58af0f027823d0532c5
arvin855 <38344661+arvin855@users.noreply.github.com>
2021-10-21 00:01:52 +0530

    geres and artists data fetch

--- frontend/src/screens/home/Home.js
+++ frontend/src/screens/home/Home.js
@@ -22,6 +22,7 @@ import { makeStyles } from "@material-ui/styles"
 const Home = (props) => {
   const [data, setdata] = useState([])
 
+  // get movies details
   useEffect(async () => {
     const rawResponse = await fetch(props.baseUrl + "movies", {
       method: "GET",
@@ -30,11 +31,49 @@ const Home = (props) => {
         "Content-Type": "application/json;charset=UTF-8"
       }
     })
-
     const result = await rawResponse.json()
     const item = result.movies
     setdata(item)
   }, [])
+
+  const [genres, setgenres] = useState([])
+  const [artists, setartists] = useState([])
+
+  // get Genres data
+  useEffect(() => {
+    async function gendata() {
+      const rawResponse = await fetch(props.baseUrl + "genres", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json;charset=UTF-8"
+        }
+      })
+      const result = await rawResponse.json()
+      const gen = result.genres
+      setgenres(gen)
+    }
+    gendata()
+  }, [])
+
+  // get allartists data
+  useEffect(() => {
+    async function artistsdata() {
+      const rawResponse = await fetch(props.baseUrl + "artists", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json;charset=UTF-8"
+        }
+      })
+      const result = await rawResponse.json()
+      const art = result.artists
+      setartists(art)
+    }
+    artistsdata()
+  }, [])
+  console.log(genres)
+  console.log(artists)
 
   const useStyles = makeStyles(() => ({
     title: {
@@ -49,6 +88,20 @@ const Home = (props) => {
   const handleChange = (event) => {
     setAge(event.target.value)
   }
+
+  // const [addfilter, setfilter] = useState({
+  //   movie_name: "",
+  //   first_name: "",
+  //   last_name: "",
+  //   mobile_number: "",
+  //   password: ""
+  // })
+
+  // const inputChangedHandler = (e) => {
+  //   const state = addUser
+  //   state[e.target.name] = e.target.value
+  //   setfilter({ ...state })
+  // }
 
   return (
     <Fragment>
@@ -100,6 +153,27 @@ const Home = (props) => {
               color="theme.palette.primary.light"
               style={{ color: "theme.palette.primary.light" }}
             />
+
+            <CardContent margin="theme.spacing.unit">
+              <FormControl
+                variant="standard"
+                style={{ minWidth: 240, maxWidth: 240 }}
+              >
+                <InputLabel htmlFor="genres">Genres</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="genres"
+                  value={age}
+                  label="Age"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </CardContent>
+
             <CardContent margin="theme.spacing.unit">
               <FormControl
                 variant="standard"
@@ -118,7 +192,7 @@ const Home = (props) => {
                 <InputLabel htmlFor="genres">Genres</InputLabel>
                 <Select
                   labelId="demo-simple-select-label"
-                  id="demo-simple-select"
+                  id="genres"
                   value={age}
                   label="Age"
                   onChange={handleChange}
@@ -135,10 +209,10 @@ const Home = (props) => {
                 variant="standard"
                 style={{ minWidth: 240, maxWidth: 240 }}
               >
-                <InputLabel htmlFor="genres">Artists</InputLabel>
+                <InputLabel>Artists</InputLabel>
                 <Select
                   labelId="demo-simple-select-label"
-                  id="demo-simple-select"
+                  id="Artists"
                   value={age}
                   label="Age"
                   onChange={handleChange}
